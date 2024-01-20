04e6ea858b0f0a9f02ec6e3ee27d802f575a0096
Monika M <monika22109@gmail.com>
2024-01-20 11:03:37 +0530

    EDA and project statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys;
-import logging;
+from src.logger import logging;
 #import logger;
 
 def error_message_detail(error,error_detail:sys):
@@ -16,4 +16,9 @@ class CustomException(Exception):
     def __str__(self) -> str:
         return self.error_message
     
-
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Devide By zero")
+        raise CustomException(e,sys)
